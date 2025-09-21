1d6f2adc476f4e12925b3f5d9e8b6c9bdb4b7236
Artemas <arden@lanth.me>
2025-09-21 21:49:42 +0200

    Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import {StrictMode} from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Home from './pages/Home.tsx'
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Analytics />
+    <SpeedInsights />
     <Home />
   </StrictMode>,
 )
